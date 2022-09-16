fc9a6ca4a1ac47c52c3e48371de33b6d8a92bc6e
William Johansson <william.johansson@einride.tech>
2022-09-16 16:29:50 +0200

    feat: allow providing a IDGenerator function
    For services with user-settable IDs, there might be requirements on how
    the format of that ID should look. Providing this function, i.e. it is
    non-nil, will call it and fill in the returned ID during creation steps.
    
    It is expected (and documented) that the function should return unique
    IDs. It is up to the user to fulfill this requirement. One provided
    example is to use a counter and include that in the ID.
    
    This feature makes it possible to use this test suite for APIs where
    user-settable ID is required, which wasn't possible before.

--- example/freight_service_test.go
+++ example/freight_service_test.go
@@ -2,6 +2,8 @@ package example
 
 import (
 	"context"
+	"fmt"
+	"sync/atomic"
 	"testing"
 
 	examplefreightv1 "github.com/einride/protoc-gen-go-aip-test/proto/gen/einride/example/freight/v1"
@@ -18,6 +20,9 @@ func Test_FreightService(t *testing.T) {
 		Server: server,
 	}
 
+	// counter to keep track of unique IDs.
+	var idCounter uint64
+
 	// run tests for each resource in the service
 	ctx := context.Background()
 	suite.TestShipper(ctx, examplefreightv1.ShipperTestSuiteConfig{
@@ -28,6 +33,13 @@ func Test_FreightService(t *testing.T) {
 				DisplayName:    "Example shipper",
 				BillingAccount: "billingAccounts/12345",
 			}
+		},
+		// IDGenerator should return a valid and unique ID to use in the Create call.
+		// If non-nil, this function will be called to set the ID on all Create calls.
+		// If the ID field is required, tests will fail if this is nil.
+		IDGenerator: func() string {
+			id := atomic.AddUint64(&idCounter, 1)
+			return fmt.Sprintf("valid-id-%d", id)
 		},
 		// Update should return a resource which is valid to update, i.e.
 		// all required fields set.
